ce01b138c3666bf6cd35ba3e0c4cfb40180db06f
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-06-30 16:23:03 +0100

    caching 5

--- RESEARCH-SERVICE/src/main/java/com/example/RESEARCH_SERVICE/config/CacheConfig.java
+++ RESEARCH-SERVICE/src/main/java/com/example/RESEARCH_SERVICE/config/CacheConfig.java
@@ -1,21 +1,13 @@
 package com.example.RESEARCH_SERVICE.config;
 
-import com.example.RESEARCH_SERVICE.utils.CacheNames;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableCaching
