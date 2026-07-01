cc07ad89879bede26ae24532a80f00e1e2173542
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-07-01 17:14:34 +0100

    Idempotency 2

---
+++ RESEARCH-SERVICE/src/main/java/com/example/RESEARCH_SERVICE/entity/RequestFingerprint.java
@@ -0,0 +1,18 @@
+package com.example.RESEARCH_SERVICE.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class RequestFingerprint {
+
+    private Long userId;
+
+    private String endpoint;
+
+    private String httpMethod;
+
+    private String requestBody;
+
+}
